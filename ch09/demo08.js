b5a0a69e5a7302e9635c5b699e3f9f5bca7ae132
yinyujia <1373722811@qq.com>
2018-03-30 15:56:43 +0800

    exercise

---
+++ ch09/demo08.js
@@ -0,0 +1,5 @@
+console.log(a,b);//undefined undefined
+var b = 23;
+console.log(a,b);//undefined 23
+var a = b;
+console.log(a,b);//23 23
---
+++ ch09/demo09.js
@@ -0,0 +1,9 @@
+var name="jack";
+function echo(){
+console.log(name);
+}
+function foo(){
+var name="bill";
+echo();
+}
+foo();//jack
