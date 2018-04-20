3bdfaf67b254c17c40eb54e4f399a4788a9a8e18
yinyujia <1373722811@qq.com>
2018-04-20 17:24:14 +0800

    练习

---
+++ ch13/demo.js
@@ -0,0 +1,123 @@
+//JS对象
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        console.log(this.str);
+    }
+};
+console.log(obj.num);//10
+console.log(obj.str);//Hi
+obj.show();	//Hi
+
+//typeof
+console.log(typeof Array);//function
+console.log(typeof Function);//function
+console.log(typeof Date);//function
+console.log(typeof Number);//function
+console.log(typeof String);//function
+console.log(typeof Boolean);//function
+console.log(typeof Math);//object
+console.log(typeof JSON);//object
+
+//instanceof
+console.log(Object instanceof Function);//true
+console.log(Object instanceof Object);//true
+console.log(Boolean instanceof Function);//true
+console.log(Boolean instanceof Object);//true
+console.log(String instanceof Function);//true
+console.log(String instanceof Object);//true
+console.log(Number instanceof Function);//true
+console.log(Number instanceof Object);//true
+console.log(Function instanceof Function);//true
+console.log(Function instanceof Object);//true
+console.log(Array instanceof Function);//true
+console.log(Array instanceof Object);//true
+console.log(Date instanceof Function);//true
+console.log(Date instanceof Object);//true
+console.log(Math instanceof Function);//false
+console.log(Math instanceof Object);//true
+console.log(JSON instanceof Function);//false
+console.log(JSON instanceof Object);//true
+
+//访问器属性
+var o = {
+    _x:1.0,
+    get x(){
+        return this._x;
+    },
+    set x(val){
+        this._x = val;
+    }
+};
+console.log(o.x);//1
+o.x = 2;
+console.log(o.x,o._x);//2 2
+
+var o = {
+    _x:1.0,
+    get x(){
+        return this._x;
+    }
+};
+console.log(o.x);//1
+o.x = 2;
+console.log(o.x,o._x);//1 1
+
+var o = {
+    _x:1.0,
+    get x(){
+        return this._x;
+    }
+};
+console.log(o.x);//1
+o.x = 2;
+console.log(o.x);//1
+
+//创建对象
+//通过字面量的方式创建 JS对象
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        return this.str;
+    }
+};
+console.log(obj.num);//10
+console.log(obj.str);//Hi
+console.log(obj.show());//Hi
+console.log(obj.__proto__);
+console.log(obj.__proto__ === Object.prototype);//
+
+//通过Object工场方法创建JS对象,注：JS对象是通过原型链的方式实现的对象继承
+var newObj = Object.create(obj);//newObj的原型是obj
+newObj.age = 23;
+console.log(newObj.num);
+console.log(newObj.str);
+console.log(newObj.show());
+console.log(newObj.age);//自有属性
+console.log(newObj.__proto__);
+console.log(newObj.__proto__ === obj);
+
+//构造函数的方式创建JS对象 
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.sayName = function(){
+    console.log("hello,i'm",this.name,this.age,"years old");
+};
+var person1 = new Person("Mike",21);
+person1.sayName();//hello,i'm Mike 21 years old
+
+var objStr = new Object("xxx");
+console.log(typeof objStr);//object
+console.log(objStr instanceof String);//true
+
+var objNum = new Object(23);
+console.log(typeof objNum);//object
+console.log(objNum instanceof Number);//true
+
+var objBoolean = new Object(true);
+console.log(typeof objBoolean);//object
+console.log(objBoolean instanceof Boolean);//true
