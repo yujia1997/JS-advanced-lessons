c4d10ea0eb3833d49755c25b607d678811a7fc1e
yinyujia <1373722811@qq.com>
2018-04-26 21:53:05 +0800

    exercise

---
+++ ch15/demo.js
@@ -0,0 +1,90 @@
+//用3种方法创建对象
+var obj1,obj2,obj3;
+//obj1通过字面量方式创建，并创建自身属性x
+//obj2通过Object静态方法创建，obj2的原型为obj1，并创建自身属性y
+//obj3通过构造函数来创建，构造函数为Obj3
+
+//通过字面量的方式创建 JS对象
+var obj = {
+    num:10,
+    str:"Hi",
+    show:function(){
+        return this.str;
+    }
+};
+console.log(obj.num);//10
+console.log(obj.str);//Hi
+console.log(obj.show());//Hi
+
+//通过Object工场方法创建JS对象,注：JS对象是通过原型链的方式实现的对象继承
+var newObj = Object.create(obj);
+newObj.age = 23;
+console.log(newObj.num);//10
+console.log(newObj.str);//Hi
+console.log(newObj.show());//Hi
+console.log(newObj.age);//自有属性 23
+
+var empty = {};
+var obj2 = Object.create(empty,{
+   x:{value:1}, y:{value:2,enumerable:true}
+});
+console.log(obj2);//{y:2 x:1}
+console.log(obj2.hasOwnProperty("x"));//true
+
+//构造函数的方式创建JS对象
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.sayName = function(){
+    console.log("hello,i'm",this.name,this.age,"years old");
+};
+
+var person1 = new Person("Mike",21);
+person1.sayName();//hello,i'm Mike 21 years old
+
+//原型链综述
+var proObj = {
+    z:3
+};
+
+var obj = Object.create(proObj);
+obj.x = 1;
+obj.y = 2;
+
+console.log(obj.x);//1
+console.log(obj.y);//2
+console.log(obj.z);//3
+
+console.log("z" in obj);//true
+console.log(obj.hasOwnProperty("z"));//false
+
+//原型链属性操作
+obj.z = 5;
+
+console.log(obj.hasOwnProperty("z"));
+console.log(obj.z);
+console.log(proObj.z);
+
+obj.z = 8;
+console.log(obj.z);
+
+delete obj.z;//true
+console.log(obj.z);
+
+delete obj.z;//true
+console.log(obj.z);//3
+
+function MyObj() { }
+MyObj.prototype.z = 3;
+
+var obj = new MyObj();
+obj.x = 1;
+obj.y = 2;
+
+console.log(obj.x);//1
+console.log(obj.y);//2
+console.log(obj.z);//3
+
+console.log("z" in obj);//true
+console.log(obj.hasOwnProperty("z"));//false
