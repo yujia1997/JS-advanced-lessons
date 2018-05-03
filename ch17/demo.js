548a8fab7062bd19c1786d5d95512469a210ea5f
yinyujia <1373722811@qq.com>
2018-05-03 21:27:05 +0800

    exercise

---
+++ ch17/demo.js
@@ -0,0 +1,47 @@
+//通过Object.create静态方法创建的对象的原型共享问题
+var superObj = {
+    x:1,
+    y:2
+};
+var subObj_First = Object.create(superObj);
+var subObj_Second = Object.create(superObj);
+subObj_First.__proto__.x = 5;
+//若此行写为subObj_First.x = 5;结果又是如何？//1
+console.log(subObj_Second.x);//5
+
+//通过构造函数创建的对象的原型共享问题
+function Person(name){
+    this.name = name;
+}
+Person.prototype.age = 22;
+Person.prototype.showName = function(){console.log(this.name);};
+function Student(id){
+    this.id = id;
+}
+
+//JS实现继承的形式 一
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){console.log(this.name);};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
+Student.prototype.__proto__ = Person.prototype;
+var s1 = new Student("xxx",22,2017001);
+var s2 = new Student("www",23,2017002);
+
+//JS实现继承的形式 二
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+};
+Person.prototype.showName = function(){
+    console.log(this.name);
+};
+function Student(name,age,id){
+    Person.call(this,name,age);
+    this.id = id;
+}
